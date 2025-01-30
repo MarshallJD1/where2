1b5b3f9c44151ceb1369dc55b18b9ec7755af2a0
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-30 22:58:15 +0000

    add basic user sign up, sign in and signout functionality

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from . import views
 from django.urls import path
+from .views import register_view, login_view, logout_view
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', 
-        views.HomePage.as_view(), name='home'),
+    path('', views.HomePage.as_view(), name='home'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
